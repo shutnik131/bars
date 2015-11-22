5ab4a281c5fc3a7c7455e5324d3416c46bb1f8a8
shutnik131 <shutnik131@yandex.ru>
2015-11-23 02:19:47 +0500

    Проработать структуру проекта, подумать об ajax, extjs. Добалена форма авторизации, изменена регистрация пользователя, добавлена привязка заметок к аккаунту.

--- profiles/notes/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Author
-
-class RegisterForm(forms.ModelForm):
-    password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
-    password1 = forms.CharField(label='Пароль(еще раз)',widget=forms.PasswordInput)
-    class Meta():
-        model = Author
-        fields = ['username', 'password', 'password1']
--- profiles/notes/views.py
+++ profiles/notes/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib import auth
@@ -16,6 +16,25 @@ def register(request):
     else:
         form = UserCreationForm()
     return render_to_response('register.html', {'form': form}, RequestContext(request))
+
+
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect('/notes/')
+            else:
+                return HttpResponse("Ваш аккаунт неактивен")
+        else:
+            return HttpResponse("Неправильная пара логин/пароль.")
+    else:
+        form = AuthenticationForm()
+    return render_to_response('login.html', {'form': form}, RequestContext(request))
 
 
 
