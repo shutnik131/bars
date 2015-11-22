577e9e04b57a0faef24cdb3aead5949585fb7ca7
shutnik131 <shutnik131@yandex.ru>
2015-11-22 15:31:53 +0500

    Изменение формы

--- profiles/users/forms.py
+++ profiles/users/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Author
 
 class RegisterForm(forms.ModelForm):
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
     password1 = forms.CharField(label='Пароль(еще раз)',widget=forms.PasswordInput)
     class Meta():
         model = Author
