dafcd96d892d0c09dbe2ac0ff22521291b7d9dc4
shutnik131 <shutnik131@yandex.ru>
2015-11-23 12:07:49 +0500

    Добавлена форма заметки

--- profiles/notes/urls.py
+++ profiles/notes/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = (
     url(r'^$', views.register),
-    url(r'^login/$', views.login),
-    url(r'notes/$', views.notes),
+    url(r'^login/$', views.login, name='login'),
+    url(r'add_note/$', views.add_notes),
+    url(r'^notes/', views.notes),
 )
--- profiles/notes/views.py
+++ profiles/notes/views.py
@@ -1,8 +1,12 @@
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, render
 from django.template import RequestContext
 from django.contrib import auth
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .forms import NoteForm
+from django.contrib import messages
 
 
 # Create your views here.
@@ -12,11 +16,10 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            return HttpResponseRedirect("/notes/")
+            return HttpResponseRedirect("/login/")
     else:
         form = UserCreationForm()
     return render_to_response('register.html', {'form': form}, RequestContext(request))
-
 
 
 def login(request):
@@ -27,16 +30,29 @@ def login(request):
         if user is not None:
             if user.is_active:
                 auth.login(request, user)
-                return HttpResponseRedirect('/notes/')
+                request.session['user_id'] = user.id
+                return HttpResponseRedirect('/add_note/')
             else:
                 return HttpResponse("Ваш аккаунт неактивен")
         else:
-            return HttpResponse("Неправильная пара логин/пароль.")
+            messages.add_message(request, messages.INFO, "Неправильный логин или пароль!")
+            return HttpResponseRedirect(reverse('login'))
     else:
         form = AuthenticationForm()
     return render_to_response('login.html', {'form': form}, RequestContext(request))
 
 
+@login_required
+def add_notes(request):
+    if request.method == "POST":
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/notes/")
+    else:
+        form = NoteForm()
+    return render_to_response('add_note.html', {'form': form}, RequestContext(request))
+
 
 def notes(request):
-    return render_to_response('notes.html')
+    return render_to_response('notes')
